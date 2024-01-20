d1c116a3c202b819433f1271fd5650433b191ea1
rodrigoDev <rodriguinho2456@gmail.com>
2024-01-20 20:34:26 -0300

    feat - Random String

---
+++ src/main/java/com/rodrigoSilva/paymentsystem/util/RandomString.java
@@ -0,0 +1,18 @@
+package com.rodrigoSilva.paymentsystem.util;
+
+import java.security.SecureRandom;
+
+public class RandomString {
+
+    private static  final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    public static String generateRandomString(int length){
+        SecureRandom random = new SecureRandom();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            int index = random.nextInt(CHARACTERS.length());
+            sb.append(CHARACTERS.charAt(index));
+        }
+        return sb.toString();
+    }
+}
