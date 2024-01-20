0fd45d793d5a54b8ca93c69f291f96f9ece530e1
rodrigoDev <rodriguinho2456@gmail.com>
2024-01-20 20:26:54 -0300

    initial project

---
+++ src/main/java/com/rodrigoSilva/paymentsystem/PaymentSystemApplication.java
@@ -0,0 +1,14 @@
+package com.rodrigoSilva.paymentsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+@SpringBootApplication
+public class PaymentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rodrigoSilva/paymentsystem/PaymentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigoSilva.paymentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
