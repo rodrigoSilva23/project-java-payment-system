d96cc3cb59007736649f829cd184b374ebf8d7cc
rodrigoDev <rodriguinho2456@gmail.com>
2024-01-20 20:50:22 -0300

    feat - created user

--- src/main/java/com/rodrigoSilva/paymentsystem/PaymentSystemApplication.java
+++ src/main/java/com/rodrigoSilva/paymentsystem/PaymentSystemApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {SecurityAutoConfiguration.class})
 public class PaymentSystemApplication {
 
 	public static void main(String[] args) {
